a9986b6813662b76914c4563d13d5a653a6dcacd
Nightmare-MY <906262255@qq.com>
2020-07-31 21:05:35 +0800

    增加机型

--- lib/utils/device_utils.dart
+++ lib/utils/device_utils.dart
@@ -3,6 +3,7 @@ class DeviceUtils {
   static final Map<String, String> _map = <String, String>{
     'platina': '小米8青春版',
     'umi': '小米10',
+    'santoni': 'redmi4x',
   };
   static String getName(String id) {
     return _map[id] ?? id;
