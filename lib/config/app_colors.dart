1f361c0cef156c0a03a76874c2b0e86fa6355fba
Nightmare-MY <906262255@qq.com>
2021-05-03 15:53:50 +0800

    add color config

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'candy_colors.dart';
+
+class AppColors {
+  static Color fontTitle = Color(0xff0d0d14);
+
+  // static Color accent = const Color(0xff3573e0);
+  static Color accent = CandyColors.deepPurple;
+  static Color fontDetail = Color(0xff707076);
+  static Color contentBorder = Color(0xfff0f1f3);
+}
